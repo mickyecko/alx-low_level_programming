217f158e93d09f84d9177263d161018ff2ad1b58
mickyecko <onahowoichomike@gmail.com>
2022-10-03 14:10:16 -0700

    task

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * create_array - creates an array of chars, and
+ * initializes it with a character c.
+ * @size: size of array
+ * @c: array initializer
+ * Author: Michael Owoicho Onah
+ * Return: pointer to array or NULL if size is 0.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = NULL;
+	unsigned int i;
+
+	/* set array size*/
+	if (size != 0)
+	{
+		array = (char *) malloc(size * sizeof(char));
+		/* fill array with c */
+		if (array != NULL)
+		{
+			for (i = 0; i < size; i++)
+				array[i] = c;
+		}
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+all my task are competed
